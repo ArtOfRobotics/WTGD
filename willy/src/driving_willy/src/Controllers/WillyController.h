675192dc5efe65ab591179e25ea7bada53dea8e5
Martijn van Olst <martijn_van_olst@hotmail.com>
2018-03-14 11:18:27 +0100

    Formatted code

--- willy/src/driving_willy/src/Controllers/WillyController.h
+++ willy/src/driving_willy/src/Controllers/WillyController.h
@@ -17,88 +17,89 @@ struct SonarCheck
 
 class WillyController
 {
-	public:
-		//Constructor
-		WillyController();
+  public:
+	//Constructor
+	WillyController();
 
-		//get en set  fuctions for Latitude and Longitude
-		double getLng();
-		double getLat();
-		int getSat();
-		void setLng(double lng);
-		void setLat(double lat);
-		void setSat(int sat);
-		//Method which executes the given command.
-		void Execute(ICommand&);
+	//get en set  fuctions for Latitude and Longitude
+	double getLng();
+	double getLat();
+	int getSat();
+	void setLng(double lng);
+	void setLat(double lat);
+	void setSat(int sat);
+	//Method which executes the given command.
+	void Execute(ICommand &);
 
-		//Method which handles the given sonar feedback.
-		void SonarCallback(const sensor_msgs::LaserEcho& sonar);
+	//Method which handles the given sonar feedback.
+	void SonarCallback(const sensor_msgs::LaserEcho &sonar);
 
-		//Method which handles the given feedback from the arduino.
-		void WheelCallback(const geometry_msgs::Vector3::ConstPtr& ticks);
+	//Method which handles the given feedback from the arduino.
+	void WheelCallback(const geometry_msgs::Vector3::ConstPtr &ticks);
 
-		void GpsCallback(const std_msgs::String::ConstPtr& msg);
+	void GpsCallback(const std_msgs::String::ConstPtr &msg);
 
-		//Method which calculates the distances.
-		int ReadDepthData(unsigned int height_pos, unsigned int width_pos, sensor_msgs::ImageConstPtr depth_image);
+	//Method which calculates the distances.
+	int ReadDepthData(unsigned int height_pos, unsigned int width_pos, sensor_msgs::ImageConstPtr depth_image);
 
-		//Method which calculates the distances by an array.
-		int ReadDepthDataArray(sensor_msgs::ImageConstPtr depth_image);
+	//Method which calculates the distances by an array.
+	int ReadDepthDataArray(sensor_msgs::ImageConstPtr depth_image);
 
-		//Method where the ROS Node can be given.
-		void SetNode(ros::NodeHandle *n);
+	//Method where the ROS Node can be given.
+	void SetNode(ros::NodeHandle *n);
 
-		//Method where a command can be given to the cmd_vel
-		void SendCommandToArduino(geometry_msgs::Twist msg);
+	//Method where a command can be given to the cmd_vel
+	void SendCommandToArduino(geometry_msgs::Twist msg);
 
-		//Method which returns the _ticks property
-		geometry_msgs::Vector3 GetLatestTicks();
+	//Method which returns the _ticks property
+	geometry_msgs::Vector3 GetLatestTicks();
 
-		//Method for returning the value by the degrees
-		int GetSonarValueByDegrees(int Degrees);
+	//Method for returning the value by the degrees
+	int GetSonarValueByDegrees(int Degrees);
 
-		//Calculating
-		void CalculateMovingPossibilities();
+	//Calculating
+	void CalculateMovingPossibilities();
 
-		//Checks if the Controller received feedback from the Encoders.
-		//Will be default false, after first WheelCallback true.
-		bool ReceivedFirstTick;
+	//Checks if the Controller received feedback from the Encoders.
+	//Will be default false, after first WheelCallback true.
+	bool ReceivedFirstTick;
 
-		//Depth mm in front of the robot.
-		int DepthInFront;
+	//Depth mm in front of the robot.
+	int DepthInFront;
 
-		//Property which stores if he can drive forward
-		bool CanDriveForward;
-		bool CanDriveBackward;
-		bool CanTurnLeft;
-		bool CanTurnRight;
+	//Property which stores if he can drive forward
+	bool CanDriveForward;
+	bool CanDriveBackward;
+	bool CanTurnLeft;
+	bool CanTurnRight;
 
-		//Array where the pointers in the array are where he reads the depth
-		int ReadPointsInImage[10][2];
+	//Array where the pointers in the array are where he reads the depth
+	int ReadPointsInImage[10][2];
 
-		//Array with data of sonars.
-		Sonar SonarData[10];
+	//Array with data of sonars.
+	Sonar SonarData[10];
 
-		int ChecksTurnLeftElements;
-		SonarCheck ChecksTurnLeft[5];
+	int ChecksTurnLeftElements;
+	SonarCheck ChecksTurnLeft[5];
 
-		int ChecksTurnRightElements;
-		SonarCheck ChecksTurnRight[5];
+	int ChecksTurnRightElements;
+	SonarCheck ChecksTurnRight[5];
 
-		int ChecksDriveForwardElements;
-		SonarCheck ChecksDriveForward[5];
+	int ChecksDriveForwardElements;
+	SonarCheck ChecksDriveForward[5];
 
-		int ChecksDriveBackwardElements;
-		SonarCheck ChecksDriveBackward[5];
-	private:
-		//Latitude and longitude
-		float lat, lng;
-		//property which contains latest ticks of the encoder on willy.
-		geometry_msgs::Vector3 _ticks;
+	int ChecksDriveBackwardElements;
+	SonarCheck ChecksDriveBackward[5];
 
-		//The publisher where commands can be given throught cmd_vel
-		ros::Publisher _commandPublisher;
-		ros::Publisher _movingPossibilitiesPublisher;
+  private:
+	//Latitude and longitude
+	float lat, lng;
+	//property which contains latest ticks of the encoder on willy.
+	geometry_msgs::Vector3 _ticks;
+
+	//The publisher where commands can be given throught cmd_vel
+	ros::Publisher _commandPublisher;
+	ros::Publisher _movingPossibilitiesPublisher;
 };
 
 #endif
--- willy/src/driving_willy/src/main.cpp
+++ willy/src/driving_willy/src/main.cpp
@@ -3,106 +3,108 @@
 #include "Includes/PUGIXML/pugixml.hpp"
 #include "Includes/PUGIXML/pugixml.cpp"
 #include "Includes/PUGIXML/pugiconfig.hpp"
-#define Node pugi::xml_node&
+#define Node pugi::xml_node &
 #define TreeWalker pugi::xml_tree_walker
 
 //Set the controller as a global variable.
 WillyController controller;
 
 // tag::impl[]
-struct ConfigIterator: TreeWalker
+struct ConfigIterator : TreeWalker
 {
     virtual bool for_each(Node node)
     {
-    	string type = node.attribute("type").value();
+        string type = node.attribute("type").value();
 
-		if(type == "TurnLeft") {
-			SonarCheck check;
+        if (type == "TurnLeft")
+        {
+            SonarCheck check;
 
-			check.SonarID = node.attribute("SonarID").as_int();
-			check.Value = node.attribute("Value").as_int();
+            check.SonarID = node.attribute("SonarID").as_int();
+            check.Value = node.attribute("Value").as_int();
 
-			controller.ChecksTurnLeft[controller.ChecksTurnLeftElements] = check;
-			controller.ChecksTurnLeftElements++;
-		}
+            controller.ChecksTurnLeft[controller.ChecksTurnLeftElements] = check;
+            controller.ChecksTurnLeftElements++;
+        }
 
-		if(type == "TurnRight") {
-			SonarCheck check;
+        if (type == "TurnRight")
+        {
+            SonarCheck check;
 
-			check.SonarID = node.attribute("SonarID").as_int();
-			check.Value = node.attribute("Value").as_int();
+            check.SonarID = node.attribute("SonarID").as_int();
+            check.Value = node.attribute("Value").as_int();
 
-			controller.ChecksTurnRight[controller.ChecksTurnRightElements] = check;
-			controller.ChecksTurnRightElements++;
-		}
+            controller.ChecksTurnRight[controller.ChecksTurnRightElements] = check;
+            controller.ChecksTurnRightElements++;
+        }
 
-		if(type == "DriveForward") {
-			SonarCheck check;
+        if (type == "DriveForward")
+        {
+            SonarCheck check;
 
-			check.SonarID = node.attribute("SonarID").as_int();
-			check.Value = node.attribute("Value").as_int();
+            check.SonarID = node.attribute("SonarID").as_int();
+            check.Value = node.attribute("Value").as_int();
 
-			controller.ChecksDriveForward[controller.ChecksDriveForwardElements] = check;
-			controller.ChecksDriveForwardElements++;
-		}
+            controller.ChecksDriveForward[controller.ChecksDriveForwardElements] = check;
+            controller.ChecksDriveForwardElements++;
+        }
 
+        if (type == "DriveBackward")
+        {
+            SonarCheck check;
 
-		if(type == "DriveBackward") {
-			SonarCheck check;
+            check.SonarID = node.attribute("SonarID").as_int();
+            check.Value = node.attribute("Value").as_int();
 
-			check.SonarID = node.attribute("SonarID").as_int();
-			check.Value = node.attribute("Value").as_int();
-
-			controller.ChecksDriveBackward[controller.ChecksDriveBackwardElements] = check;
-			controller.ChecksDriveBackwardElements++;
-		}
+            controller.ChecksDriveBackward[controller.ChecksDriveBackwardElements] = check;
+            controller.ChecksDriveBackwardElements++;
+        }
         return true;
     }
 };
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 
-  pugi::xml_document ConfigFile;
-  ConfigFile.load_file("/home/willy/Documents/driving-willy/WTGD/willy/src/driving_willy/src/tree.xml");
+    pugi::xml_document ConfigFile;
+    ConfigFile.load_file("/home/willy/Documents/driving-willy/WTGD/willy/src/driving_willy/src/tree.xml");
 
-  ConfigIterator walker;
-  ConfigFile.traverse(walker);
+    ConfigIterator walker;
+    ConfigFile.traverse(walker);
 
-  //Ros initation.
-  ros::init(argc, argv, "DrivingWilly");
-  ros::NodeHandle n;
-  //Transform odometryController = Transform(&n);
-  //placeholder values for gps route.
-  std::vector<double> gpsLat;
-  std::vector<double> gpsLong;
-  gpsLat.push_back(12.4235);
-  gpsLat.push_back(13.2355);
-  n.setParam("gpsLat",gpsLat);
-  gpsLong.push_back(10.4235);
-  gpsLong.push_back(11.2355);
-  n.setParam("gpsLong",gpsLong);
+    //Ros initation.
+    ros::init(argc, argv, "DrivingWilly");
+    ros::NodeHandle n;
+    //Transform odometryController = Transform(&n);
+    //placeholder values for gps route.
+    std::vector<double> gpsLat;
+    std::vector<double> gpsLong;
+    gpsLat.push_back(12.4235);
+    gpsLat.push_back(13.2355);
+    n.setParam("gpsLat", gpsLat);
+    gpsLong.push_back(10.4235);
+    gpsLong.push_back(11.2355);
+    n.setParam("gpsLong", gpsLong);
 
-  //Set up the subsriber of the wheel encoders to the WheelCallback of the WillyController.
-  //ros::Subscriber subWheelEncoderWillyController = n.subscribe("/wheel_encoder", 200, &Transform::WheelCallback, &odometryController);
+    //Set up the subsriber of the wheel encoders to the WheelCallback of the WillyController.
+    //ros::Subscriber subWheelEncoderWillyController = n.subscribe("/wheel_encoder", 200, &Transform::WheelCallback, &odometryController);
 
-  //Set up the subscriber for the GPS
-  ros::Subscriber gpsSubscriber = n.subscribe("/gps", 200, &WillyController::GpsCallback, &controller);
+    //Set up the subscriber for the GPS
+    ros::Subscriber gpsSubscriber = n.subscribe("/gps", 200, &WillyController::GpsCallback, &controller);
 
-  //Set up the subscriber for the sonar
-  ros::Subscriber subSonar = n.subscribe("/sonar", 100, &WillyController::SonarCallback, &controller);
-  //Gives the node to the controller.
-  controller.SetNode(&n);
+    //Set up the subscriber for the sonar
+    ros::Subscriber subSonar = n.subscribe("/sonar", 100, &WillyController::SonarCallback, &controller);
+    //Gives the node to the controller.
+    controller.SetNode(&n);
 
-  //Set the asynchronised spinner for ros.
-  ros::AsyncSpinner spinner(4);
-  spinner.start();
+    //Set the asynchronised spinner for ros.
+    ros::AsyncSpinner spinner(4);
+    spinner.start();
 
-  AutonomousDrivingController autonomouseDriving = AutonomousDrivingController(&controller,&n);
-  autonomouseDriving.Start();
+    AutonomousDrivingController autonomouseDriving = AutonomousDrivingController(&controller, &n);
+    autonomouseDriving.Start();
 
-	// Wait
-	ros::waitForShutdown();
-	return 0;
+    // Wait
+    ros::waitForShutdown();
+    return 0;
 }
