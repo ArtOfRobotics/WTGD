1102e413f485f9b0bff2374ca0526072c3dd5315
Martijn van Olst <martijn_van_olst@hotmail.com>
2018-03-14 11:07:05 +0100

    Clean up

--- csharp/Willy.Web/Entities/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Willy.Web.Entities
-{
-    public class ApplicationUser : IdentityUser
-    {
-    }
-}
--- web_angular/src/app/shared/command.factory.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('app').factory('Command', function ($resource, ApiUrl) {
-        return $resource(ApiUrl + 'controlpanel/commands/:commandId', {commandId: '@CommandId'});
-    });
-
-
-})();
