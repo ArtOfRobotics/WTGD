541df4f64a354e213b1f8d8b95b125b9e7926651
lalilaloe <jonathantenhove@hotmail.com>
2018-03-26 14:52:06 +0200

    gitsync

--- bin/gitsync.sh
+++ bin/gitsync.sh
@@ -3,6 +3,8 @@
 date
 cd /home/willy/Documents/WTGD
 if git fetch; then
+    echo "No remote changes found in WTDG"
+else
     echo "Remote changes found in WTGD"
     git pull origin test
     cd willy
@@ -10,13 +12,11 @@ if git fetch; then
     source devel/setup.bash
     cd ..
     chmod 777 bin/gitsync.sh
-else
-    echo "No remote changes found in WTDG"
 fi
 cd /home/willy/Documents/WWEB
 if git fetch; then
+    echo "No remote changes found in WWEB"
+else
     echo "Remote changes found in WWEB"
     git pull origin test
-else
-    echo "No remote changes found in WWEB"
 fi
