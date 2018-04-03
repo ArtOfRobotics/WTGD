7e69c4ce7565f43fe827f5ee04fb0603b8ac3d61
Martijn van Olst <martijn_van_olst@hotmail.com>
2018-04-03 11:55:29 +0200

    Added status_ok check

--- willy/src/driving_willy/src/Controllers/WillyController.cpp
+++ willy/src/driving_willy/src/Controllers/WillyController.cpp
@@ -34,7 +34,7 @@ WillyController::WillyController()
 	//Load sonar array data from xml
 	pugi::xml_document doc;
 	pugi::xml_parse_result result = doc.load_file("../tree.xml");
-	if (result.status != status_ok) {
+	if (result.status != result.status.status_ok) {
 		printf("Laden van bestand is mislukt");
 	}
 
