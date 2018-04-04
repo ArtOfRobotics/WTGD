4309f28126506df9ebdf293be73b935badf8cf7b
Martijn van Olst <martijn_van_olst@hotmail.com>
2018-04-04 13:59:55 +0200

    Removed old library. Updated kinect controller

--- willy/src/driving_willy/src/Controllers/KinectController.cpp
+++ willy/src/driving_willy/src/Controllers/KinectController.cpp
@@ -1,9 +1,38 @@
 #include "../include.h"
 
-KinectController::KinectController(int argc, char **argv)
+KinectController::KinectController(int argc, char **argv, ros::NodeHandle *n)
 {
+    nh = n;
 }
 
 void KinectController::Start()
 {
+    std::cout << "Oh hai there!" << std::endl;
+
+    // ros::Subscriber sub = nh.subscribe("camera/rgb/image_raw", MY_ROS_QUEUE_SIZE, imgcb);
+    ros::Subscriber sub = nh.subscribe("camera/rgb/image_color", MY_ROS_QUEUE_SIZE, imgcb);
+
+    cv::namedWindow("DrivingWilly");
+    ros::spin();
+    cv::destroyWindow("DrivingWilly");
+
+    std::cout << "byebye my friend" << std::endl;
+}
+
+void imgcb(const sensor_msgs::Image::ConstPtr &msg)
+{
+    std::cout << "Hey, listen!" << std::endl;
+
+    try
+    {
+        cv_bridge::CvImageConstPtr cv_ptr;
+        cv_ptr = cv_bridge::toCvShare(msg);
+
+        cv::imshow("DrivingWilly", cv_ptr->image);
+        cv::waitKey(1); // Update screen
+    }
+    catch (const cv_bridge::Exception &e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+    }
 }
