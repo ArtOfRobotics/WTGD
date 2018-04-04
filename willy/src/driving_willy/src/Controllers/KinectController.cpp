742d786714d3155569f2212fc73b46fcaa3dcc8b
Martijn van Olst <martijn_van_olst@hotmail.com>
2018-04-04 15:16:34 +0200

    updated controller

--- willy/src/driving_willy/src/Controllers/KinectController.cpp
+++ willy/src/driving_willy/src/Controllers/KinectController.cpp
@@ -12,9 +12,9 @@ void KinectController::Start()
     // ros::Subscriber sub = nh.subscribe("camera/rgb/image_raw", MY_ROS_QUEUE_SIZE, imgcb);
     //ros::Subscriber sub = nh->subscribe("camera/rgb/image_color", MY_ROS_QUEUE_SIZE, imgcb);
 
-    cv::namedWindow("DrivingWilly");
-    ros::spin();
-    cv::destroyWindow("DrivingWilly");
+    //cv::namedWindow("DrivingWilly");
+    //ros::spin();
+    //cv::destroyWindow("DrivingWilly");
 
     std::cout << "byebye my friend" << std::endl;
 }
@@ -27,8 +27,8 @@ void KinectController::KinectCallback(const sensor_msgs::Image::ConstPtr &msg)
         cv_bridge::CvImageConstPtr cv_ptr;
         cv_ptr = cv_bridge::toCvShare(msg);
 
-        cv::imshow("DrivingWilly", cv_ptr->image);
-        cv::waitKey(1); // Update screen
+        //cv::imshow("DrivingWilly", cv_ptr->image);
+        //cv::waitKey(1); // Update screen
     }
     catch (const cv_bridge::Exception &e)
     {
