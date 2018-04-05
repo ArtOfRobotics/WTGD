e300f956cf99c184d8b1caf7d53b4c5671a006ce
root <willythegarbagerobot@gmail.com>
2018-04-05 11:54:10 +0200

    change

--- bin/gitsync.sh
+++ bin/gitsync.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
 
-date
 cd /home/willy/Documents/WTGD
-if git fetch > /dev/null; then
-    echo "No remote changes found in WTDG"
-else
-    echo "Remote changes found in WTGD"
+changed=0
+git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+if [ $changed = 1 ]; then    
+	echo "Remote changes found in WTGD"
     git pull origin test
     #cd willy
     #catkin_make
     #source devel/setup.bash
     #cd ..
     chmod 777 bin/gitsync.sh
+	echo "WTGD Last updated:"
+	date
+else
+    #echo "Up-to-date"
 fi
 cd /home/willy/Documents/WWEB
-if git fetch > /dev/null; then
-    echo "No remote changes found in WWEB"
-else
-    echo "Remote changes found in WWEB"
+changed=0
+git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+if [ $changed = 1 ]; then
+    
+	echo "Remote changes found in WWEB"
     git pull origin test
+	echo "WWEB Last updated:"
+	date
+else
+    #echo "Up-to-date"
 fi
