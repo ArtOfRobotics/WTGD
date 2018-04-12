6745f9ced0f4e7b931a7ff4c4131d539653b8477
Martijn van Olst <martijn_van_olst@hotmail.com>
2018-04-12 14:32:09 +0200

    update

--- willy/src/driving_willy/src/Vision/VisionController.cpp
+++ willy/src/driving_willy/src/Vision/VisionController.cpp
@@ -1,9 +1,9 @@
 #include "../include.h"
 
 using namespace std;
-void *kinect;
-void *lidar;
-void *sonar;
+uintptr_t kinect;
+uintptr_t lidar;
+uintptr_t sonar;
 
 VisionController::VisionController(ros::NodeHandle *nh, KinectController *kinectController, LidarController *lidarController, SonarController *sonarController)
 {
@@ -14,31 +14,34 @@ VisionController::VisionController(ros::NodeHandle *nh, KinectController *kinect
     //Call movement controllers if enabled
     if (useKinect)
     {
-        kinect = reinterpret_cast<void*>(kinectController);
+        kinect = reinterpret_cast<uintptr_t>(kinectController);
     }
 
     if (useLidar)
     {
-        lidar = reinterpret_cast<void*>(lidarController);
+        lidar = reinterpret_cast<uintptr_t>(lidarController);
     }
 
     if (useSonar)
     {
-        sonar = reinterpret_cast<void*>(sonarController);
+        sonar = reinterpret_cast<uintptr_t>(sonarController);
     }
 }
 
 KinectController *VisionController::GetKinectController()
 {
-    return dynamic_cast<KinectController*>(kinect);
+    void *p = reinterpret_cast<void*>(kinect);
+    return dynamic_cast<KinectController*>(p);
 }
 
 LidarController *VisionController::GetLidarController()
 {
-    return dynamic_cast<LidarController*>(lidar);
+    void *p = reinterpret_cast<void*>(lidar);
+    return dynamic_cast<LidarController*>(p);
 }
 
 SonarController *VisionController::GetSonarController()
 {
-    return dynamic_cast<SonarController*>(sonar);
+    void * = reinterpret_cast<void*>(sonar);
+    return dynamic_cast<SonarController*>(p);
 }
