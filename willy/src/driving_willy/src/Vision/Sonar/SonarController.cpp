1fac0a42465631054d98f57bd95e9cfa481e10a2
Martijn van Olst <martijn_van_olst@hotmail.com>
2018-04-16 15:05:11 +0200

    update

--- willy/src/driving_willy/src/Vision/Sonar/SonarController.cpp
+++ willy/src/driving_willy/src/Vision/Sonar/SonarController.cpp
@@ -27,7 +27,6 @@ SonarController::SonarController()
     //// |6-------7--------8|
     ////
 
-    /*
     //Load sonar array data from xml
     pugi::xml_document doc;
     pugi::xml_parse_result result = doc.load_file("src/driving_willy/src/tree.xml");
@@ -57,7 +56,7 @@ SonarController::SonarController()
             }
             else if ((strcmp(attr.name(), "type") == 0) && (strcmp(attr.value(), "DriveForward") == 0))
             {
-                printf("Er is een element toegevoegd! SonarID: %d. Value: %d.", tool.attribute("SonarID").as_int(), tool.attribute("Value").as_int());
+                //printf("Er is een element toegevoegd! SonarID: %d. Value: %d.", tool.attribute("SonarID").as_int(), tool.attribute("Value").as_int());
                 ChecksDriveForward[ForwardSensorCount].SonarID = tool.attribute("SonarID").as_int();
                 ChecksDriveForward[ForwardSensorCount].Value = tool.attribute("Value").as_int();
                 ForwardSensorCount++;
@@ -69,7 +68,7 @@ SonarController::SonarController()
                 BackwardSensorCount++;
             }
         }
-    }*/
+    }
 }
 
 //This method gets fired wen there's a new message from the sonar system.
