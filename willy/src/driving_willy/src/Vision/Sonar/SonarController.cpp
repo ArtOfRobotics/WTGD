2464c2022fee504d992f8d0bfa9662e020824cd7
Martijn van Olst <martijn_van_olst@hotmail.com>
2018-04-16 15:35:21 +0200

    update

--- willy/src/driving_willy/src/Vision/Sonar/SonarController.cpp
+++ willy/src/driving_willy/src/Vision/Sonar/SonarController.cpp
@@ -27,10 +27,10 @@ SonarController::SonarController()
     //// |6-------7--------8|
     ////
 
-    ChecksTurnLeft = new SonarCheck;
-    ChecksTurnRight = new SonarCheck;
-    ChecksDriveBackward = new SonarCheck;
-    ChecksDriveForward = new SonarCheck;
+    ChecksTurnLeft[10] = new SonarCheck[10];
+    ChecksTurnRight[10] = new SonarCheck[10];
+    ChecksDriveBackward[10] = new SonarCheck[10];
+    ChecksDriveForward[10] = new SonarCheck[10];
 
     pugi::xml_document doc;
     pugi::xml_parse_result result;
