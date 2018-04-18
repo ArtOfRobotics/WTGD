93a48c74e934b41eb5235a583d356852b3b6d7da
Martijn van Olst <martijn_van_olst@hotmail.com>
2018-04-18 11:42:59 +0200

    update

--- willy/src/driving_willy/src/Vision/Kinect/KinectController.cpp
+++ willy/src/driving_willy/src/Vision/Kinect/KinectController.cpp
@@ -8,10 +8,6 @@ KinectController::KinectController()
     printf("Er is een nieuwe Kinect controller aangemaakt!\n");
 }
 
-virtual void KinectController::onInit()
-{
-}
-
 void KinectController::KinectCallback(const sensor_msgs::Image::ConstPtr &depth_msg)
 {
 
