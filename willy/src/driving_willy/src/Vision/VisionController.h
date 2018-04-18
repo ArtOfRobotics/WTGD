90258ce6208da7cbcf0277fc565594b91eac2d9c
Martijn van Olst <martijn_van_olst@hotmail.com>
2018-04-18 11:43:53 +0200

    update

--- willy/src/driving_willy/src/Vision/VisionController.h
+++ willy/src/driving_willy/src/Vision/VisionController.h
@@ -10,15 +10,7 @@ public:
   SonarController *GetSonarController();
 
 private:
-  double min_y_;   /**< The minimum y position of the points in the box. */
-  double max_y_;   /**< The maximum y position of the points in the box. */
-  double min_x_;   /**< The minimum x position of the points in the box. */
-  double max_x_;   /**< The maximum x position of the points in the box. */
-  double max_z_;   /**< The maximum z position of the points in the box. */
-  double goal_z_;  /**< The distance away from the robot to hold the centroid */
-  double z_scale_; /**< The scaling factor for translational robot speed */
-  double x_scale_; /**< The scaling factor for rotational robot speed */
-  bool enabled_;   /**< Enable/disable following; just prevents motor commands */
+
 };
 
 #endif
