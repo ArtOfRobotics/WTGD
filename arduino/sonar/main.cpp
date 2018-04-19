18789240111147ab46fc6bc790646ea10e987ea5
Gerard Zeeman <gerardzeeman1@gmail.com>
2018-04-19 14:29:33 +0200

    Updated sensor numbers

--- arduino/sonar/main.cpp
+++ arduino/sonar/main.cpp
@@ -2,22 +2,41 @@
 #include <sensor_msgs/LaserEcho.h>
 
 //define sonar signal ports for easy configuration
-#define first_sonar_sensor    3
-#define second_sonar_sensor   4
-#define third_sonar_sensor    5
-#define fourth_sonar_sensor   6
-#define fifth_sonar_sensor    7
+#define sonar0    3
+#define sonar1    4
+#define sonar2    5
+#define sonar3    6
+#define sonar4    7
+#define sonar5    8
+#define sonar6    9   //front under left
+#define sonar7    10  //front under right
 
-#define sixth_sonar_sensor    8
-#define seventh_sonar_sensor  9
-#define eight_sonar_sensor    10
-#define nineth_sonar_sensor   11
-#define tenth_sonar_sensor    12
+#define sonar8    11
+#define sonar9    12
+#define sonar10   13
+#define sonar11   A0
+#define sonar12   A1
+#define sonar13   A2
+#define sonar14   A3  //back under left
+#define sonar15   A4  //back under right
 
-#define first_front_sensor    A0
-#define second_front_sensor   A1
-#define first_back_sensor     A2
-#define second_back_sensor    A3
+/*
+                front
+      0 -- 1 - 2 -- 3 - 4 -- 5
+      |                      |
+      |    6           7     | 
+      |                      | 
+      |                      | 
+      |      \       /       |
+      |        under         |
+      |      /       \       |
+      |                      |
+      |                      |
+      |    14          15    |
+      |                      |
+      8 - 9 - 10 - 11 - 12 - 13
+                back
+*/
 
 //function definition
 float get_measure_from(int digitalport);
@@ -29,16 +48,22 @@ ros::Publisher sonar("sonar", &message);
 
 void setup()
 {
-  pinMode(first_sonar_sensor, OUTPUT);
-  pinMode(second_sonar_sensor, OUTPUT);
-  pinMode(third_sonar_sensor, OUTPUT);
-  pinMode(fourth_sonar_sensor, OUTPUT);
-  pinMode(fifth_sonar_sensor, OUTPUT);
-  pinMode(sixth_sonar_sensor, OUTPUT);
-  pinMode(seventh_sonar_sensor, OUTPUT);
-  pinMode(eight_sonar_sensor, OUTPUT);
-  pinMode(nineth_sonar_sensor, OUTPUT);
-  pinMode(tenth_sonar_sensor, OUTPUT);
+  pinMode(sonar0, OUTPUT);
+  pinMode(sonar1, OUTPUT);
+  pinMode(sonar2, OUTPUT);
+  pinMode(sonar3, OUTPUT);
+  pinMode(sonar4, OUTPUT);
+  pinMode(sonar5, OUTPUT);
+  pinMode(sonar6, OUTPUT);
+  pinMode(sonar7, OUTPUT);
+  pinMode(sonar8, OUTPUT);
+  pinMode(sonar9, OUTPUT);
+  pinMode(sonar10, OUTPUT);
+  pinMode(sonar11, OUTPUT);
+  pinMode(sonar12, OUTPUT);
+  pinMode(sonar13, OUTPUT);
+  pinMode(sonar14, OUTPUT);
+  pinMode(sonar15, OUTPUT);
   //init ros node and advertise the publisher
   nh.initNode();
   nh.advertise(sonar);
@@ -48,33 +73,36 @@ void loop()
 {
   //get sonar values, we read the sonar in the following way
   //so there wont be any overlap in ultrasonic bursts
-  float distance_sonar_one = get_measure_from(first_sonar_sensor);
-  float distance_sonar_three = get_measure_from(third_sonar_sensor);
-  float distance_sonar_five = get_measure_from(fifth_sonar_sensor);
-  float distance_sonar_seven = get_measure_from(seventh_sonar_sensor);
-  float distance_sonar_nine = get_measure_from(nineth_sonar_sensor);
-
-  float distance_sonar_front_one = get_measure_from(first_front_sensor);
-  float distance_sonar_back_one = get_measure_from(first_back_sensor);
+ 
+  float distance0 = get_measure_from(sonar0);
+  float distance2 = get_measure_from(sonar2);
+  float distance4 = get_measure_from(sonar4);
+  float distance6 = get_measure_from(sonar6);
+  float distance8 = get_measure_from(sonar8);
+  float distance10 = get_measure_from(sonar10);
+  float distance12 = get_measure_from(sonar12);
+  float distance14 = get_measure_from(sonar14);
 
   delay(10);
 
-  float distance_sonar_two = get_measure_from(second_sonar_sensor);
-  float distance_sonar_four = get_measure_from(fourth_sonar_sensor);
-  float distance_sonar_six = get_measure_from(sixth_sonar_sensor);
-  float distance_sonar_eight = get_measure_from(eight_sonar_sensor);
-  float distance_sonar_ten = get_measure_from(tenth_sonar_sensor);
-
-  float distance_sonar_front_two = get_measure_from(second_front_sensor);
-  float distance_sonar_back_two = get_measure_from(second_back_sensor);
+  float distance1 = get_measure_from(sonar1);
+  float distance3 = get_measure_from(sonar3);
+  float distance5 = get_measure_from(sonar5);
+  float distance7 = get_measure_from(sonar7);
+  float distance9 = get_measure_from(sonar9);
+  float distance11 = get_measure_from(sonar11);
+  float distance13 = get_measure_from(sonar13);
+  float distance15 = get_measure_from(sonar15);
 
   //add sonar values to message object and publish message
-  float ech[] = {distance_sonar_one ,distance_sonar_two, distance_sonar_three,
-                 distance_sonar_four, distance_sonar_five, distance_sonar_six,
-                 distance_sonar_seven, distance_sonar_eight, distance_sonar_nine,
-                 distance_sonar_ten};
+  float ech[] = {distance0 ,distance1, distance2,
+                 distance3, distance4, distance5,
+                 distance6, distance7, distance8,
+                 distance9, distance10, distance11,
+                 distance12, distance13, distance14,
+                 distance15};
   message.echoes = ech;
-  message.echoes_length = 10;
+  message.echoes_length = 16;
   sonar.publish(&message);
 
   nh.spinOnce();
