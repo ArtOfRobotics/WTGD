0a8c32b87816d43e67b92c4b47738fdb34406f2b
Martijn van Olst <martijn_van_olst@hotmail.com>
2018-05-08 15:45:32 +0200

    update

--- willy/src/driving_willy/src/General/Light/LightController.cpp
+++ willy/src/driving_willy/src/General/Light/LightController.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+std_msgs::Bool sirenValueOn;
+std_msgs::Bool sirenValueOff;
+
 LightController::LightController()
 {
     printf("Er is een nieuwe Light controller aangemaakt!\n");
@@ -9,17 +12,19 @@ LightController::LightController()
 
 void LightController::LightCallback(const std_msgs::Char &msg)
 {
+    sirenValueOn.data = true;
+    sirenValueOff.data = false;
     std::cout << "Hey, listen!" << std::endl;
 
     if (input.data == 'o')
     {
         printf("Turn siren on\n");
-        GeneralController::GetSirenPublisher().publish(true);
+        GeneralController::GetSirenPublisher().publish(sirenValueOn);
     }
 
     if (input.data == 'p')
     {
         printf("Turn siren off\n");
-        GeneralController::GetSirenPublisher().publish(false);
+        GeneralController::GetSirenPublisher().publish(sirenValueOff);
     }
 }
